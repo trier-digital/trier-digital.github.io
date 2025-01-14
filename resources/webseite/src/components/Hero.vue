65a37ef0238c33b83509d6e8e07ec07c215383bf
homoquiestfaba <binz344@gmail.com>
2025-01-14 12:40:00 +0100

    update home

---
+++ resources/webseite/src/components/Hero.vue
@@ -0,0 +1,18 @@
+<template>
+    <div id="hero" class="hero dark-background">
+        <div class="home">
+            Trier Digital
+        </div>
+    </div>
+</template>
+
+
+<script>
+export default {
+    name: 'Hero',
+};
+</script>
+
+<style>
+/* Optional: Add your custom styles here */
+</style>

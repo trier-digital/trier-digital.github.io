5fcba1b5469982ed8c5732c439347cb40bd73466
Rachel <rachelaimeegeorg@aol.com>
2025-01-18 16:10:04 +0100

    added photo gallery and all photos

--- resources/webseite/src/views/BildergalerieView.vue
+++ resources/webseite/src/views/BildergalerieView.vue
@@ -1,5 +1,30 @@
 <template>
-    <div class="about">
-      <h1>This is an picture page</h1>
+
+    <!-- Page Title -->
+    <div class="page-title dark-background">
+    <div class="container position-relative">
+      <h1>Bildergalerie</h1>
+      <p>Esse dolorum voluptatum ullam est sint nemo et est ipsa porro placeat quibusdam quia assumenda numquam molestias.</p>
+      <nav class="breadcrumbs">
+        <ol>
+          <li><router-link to="/">Home</router-link></li>
+          <li class="current">Bildergalerie</li>
+        </ol>
+      </nav>
     </div>
-  </template>
+  </div><!-- End Page Title -->
+
+<Bildergalerie />
+</template>
+
+<script>
+// @ is an alias to /src
+import Bildergalerie from '@/components/Bildergalerie.vue';
+
+export default {
+name: 'BildergalerieView',
+components: {
+Bildergalerie
+}
+}
+</script>

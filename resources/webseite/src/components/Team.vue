037237a2f4723f97c8175a5bb4051b97210aa51e
Rachel <rachelaimeegeorg@aol.com>
2025-01-26 13:24:24 +0100

    added Team page (texts and pictures are placeholder)

---
+++ resources/webseite/src/components/Team.vue
@@ -0,0 +1,144 @@
+<template>
+   <!-- Team Section -->
+    <section id="team" class="team section">
+
+        <div class="container">
+  
+          <div class="row gy-5">
+  
+            <div class="col-lg-3 col-md-6 member">
+              <div class="member-img">
+                <img src="assets/img/team/team-1.jpg" class="img-fluid" alt="">
+              </div>
+              <div class="member-info text-center">
+                <h4>Nune Arazyan</h4>
+                <span>B.A. Kulturwissenschaft und Deutsche Literatur</span>
+                <p>Georeferenzierung historischer Karten, Trierer Routen, Recherche</p>
+              </div>
+            </div><!-- End Team Member -->
+  
+            <div class="col-lg-3 col-md-6 member">
+              <div class="member-img">
+                <img src="assets/img/team/team-2.jpg" class="img-fluid" alt="">
+              </div>
+              <div class="member-info text-center">
+                <h4>Dennis Binz</h4>
+                <span>B.A. Computerlinguistik und Phonetik,<br>B.Sc. Sprache, Technologie, Medien</span>
+                <p>Webseite</p>
+              </div>
+            </div><!-- End Team Member -->
+  
+            <div class="col-lg-3 col-md-6 member">
+              <div class="member-img">
+                <img src="assets/img/team/team-3.jpg" class="img-fluid" alt="">
+              </div>
+              <div class="member-info text-center">
+                <h4>Naemi Bürger</h4>
+                <span>B.A. Mehrsprachige Kommunikation</span>
+                <p>Recherche, Fotografien/Bildbearbeitung, Georeferenzierung</p>
+              </div>
+            </div><!-- End Team Member -->
+  
+            <div class="col-lg-3 col-md-6 member">
+              <div class="member-img">
+                <img src="assets/img/team/team-4.jpg" class="img-fluid" alt="">
+              </div>
+              <div class="member-info text-center">
+                <h4>Rebecca Daniel</h4>
+                <span>B.A. Museologie</span>
+                <p>Textkorpus</p>
+              </div>
+            </div><!-- End Team Member -->
+  
+            <div class="col-lg-3 col-md-6 member">
+              <div class="member-img">
+                <img src="assets/img/team/team-5.jpg" class="img-fluid" alt="">
+              </div>
+              <div class="member-info text-center">
+                <h4>Rachel Georg</h4>
+                <span>B.Sc. Informatik</span>
+                <p>Webseite, Recherche</p>
+              </div>
+            </div><!-- End Team Member -->
+  
+            <div class="col-lg-3 col-md-6 member">
+              <div class="member-img">
+                <img src="assets/img/team/team-6.jpg" class="img-fluid" alt="">
+              </div>
+              <div class="member-info text-center">
+                <h4>Nele Mantaj</h4>
+                <span>Karten</span>
+                <p>Sint sint eveniet explicabo amet consequatur nesciunt error enim rerum earum et omnis fugit eligendi cupiditate vel</p>
+              </div>
+            </div><!-- End Team Member -->
+  
+            <div class="col-lg-3 col-md-6 member">
+              <div class="member-img">
+                <img src="assets/img/team/team-6.jpg" class="img-fluid" alt="">
+              </div>
+              <div class="member-info text-center">
+                <h4>Bianca Ottenberg</h4>
+                <span>Diskurs: Topic Modeling</span>
+                <p>Sint sint eveniet explicabo amet consequatur nesciunt error enim rerum earum et omnis fugit eligendi cupiditate vel</p>
+              </div>
+            </div><!-- End Team Member -->
+
+            <div class="col-lg-3 col-md-6 member">
+              <div class="member-img">
+                <img src="assets/img/team/team-6.jpg" class="img-fluid" alt="">
+              </div>
+              <div class="member-info text-center">
+                <h4>Rebecca Robinson</h4>
+                <span>B.A. Literaturwissenschaften und Philosophie</span>
+                <p>Öffentlichkeitsarbeit, Recherche, Fotografien/Bildbearbeitung</p>
+              </div>
+            </div><!-- End Team Member -->
+
+            <div class="col-lg-3 col-md-6 member">
+              <div class="member-img">
+                <img src="assets/img/team/team-6.jpg" class="img-fluid" alt="">
+              </div>
+              <div class="member-info text-center">
+                <h4>Manon Rupp</h4>
+                <span>B.A. Philosophie und Geschichte</span>
+                <p>Emotionsanalyse, Visualisierungen, Recherche</p>
+              </div>
+            </div><!-- End Team Member -->
+
+            <div class="col-lg-3 col-md-6 member">
+              <div class="member-img">
+                <img src="assets/img/team/team-6.jpg" class="img-fluid" alt="">
+              </div>
+              <div class="member-info text-center">
+                <h4>Marina Spielberg</h4>
+                <span>B.A. Anglistik und Psychologie</span>
+                <p>Emotionsanalyse, Visualisierungen, Recherche</p>
+              </div>
+            </div><!-- End Team Member -->
+
+            <div class="col-lg-3 col-md-6 member">
+              <div class="member-img">
+                <img src="assets/img/team/team-6.jpg" class="img-fluid" alt="">
+              </div>
+              <div class="member-info text-center">
+                <h4>Carina Zander</h4>
+                <span>Bilder</span>
+                <p>Sint sint eveniet explicabo amet consequatur nesciunt error enim rerum earum et omnis fugit eligendi cupiditate vel</p>
+              </div>
+            </div><!-- End Team Member -->
+
+          </div>
+  
+        </div>
+  
+      </section><!-- /Team Section -->
+
+    </template>
+
+<script>
+export default {
+  name: 'Team',
+  props: {
+  }
+}
+</script>
--- resources/webseite/src/views/TeamView.vue
+++ resources/webseite/src/views/TeamView.vue
@@ -1,5 +1,48 @@
 <template>
-    <div class="about">
-        <h1>This is a team page</h1>
+    <!-- Page Title -->
+    <div class="page-title dark-background">
+    <div class="container position-relative">
+      <h1>Team</h1>
+    </div> 
+    </div><!-- End Page Title -->
+
+    <!-- Großes Team-Bild mit Text -->
+    <section id="about" class="about section light-background">
+
+  <div class="container">
+
+  <div class="row gy-4">
+    <div class="col-lg-6 position-relative align-self-start" data-aos="fade-up" data-aos-delay="100">
+      <img src="assets/img/about.jpg" class="img-fluid" alt="">
     </div>
+    <div class="col-lg-6 content" data-aos="fade-up" data-aos-delay="200">
+      <h3>Voluptatem dignissimos provident quasi corporis voluptates sit assumenda.</h3>
+      <p class="fst-italic">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
+        magna aliqua.
+      </p>
+      <p>
+        Ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident
+      </p>
+    </div>
+  </div>
+
+</div>
+
+</section><!-- /About Section -->
+
+    <Team />
 </template>
+
+<script>
+import Team from '@/components/Team.vue';
+
+export default {
+  name: "TeamView",
+  components: {
+    Team
+  }
+}
+
+</script>
